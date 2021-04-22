e7e5e2e501d927113b712b59e964ad30b488010c
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-04-22 18:03:43 -0300

    consuming api and filtering plants with a enviromnment click

---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { RectButton, RectButtonProps
+ } from 'react-native-gesture-handler'
+ import { SvgFromUri } from 'react-native-svg'
+
+interface PlantProps extends RectButtonProps {
+    data: {
+        name: string;
+        photo: string;
+    }
+}
+
+export const PlantCardPrimary = ({data, ...rest} : PlantProps) => {
+    return (
+        <RectButton style={styles.container}
+        {...rest}>
+            <SvgFromUri 
+            width={70}
+            height={70}
+            uri={data.photo}/>
+            <Text style={styles.text}>{data.name}</Text>
+        </RectButton>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+        borderRadius: 20,
+        width: 170,
+        height: 190,
+        padding: 20,
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: '#f0f5f2',
+        justifyContent: 'center'
+    },
+
+    text: {
+        color: 'green',
+        fontFamily: 'Roboto',
+        marginVertical: 16
+    }
+})
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import {Text, View, StyleSheet, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import {Text, View, StyleSheet, ScrollView } from 'react-native'
 
 import { Header } from '../components/Header'
 import { EnviromentBtn } from '../components/EnviromentBtn'
+import api from '../services/api'
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+
+interface EnviromentProps {
+    key: string;
+    title: string;
+}
+
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+        times: number;
+        repeat_every: string;
+    }
+    
+}
 
 export function PlantSelect(){
+
+    const [enviroments, setEnviroments] = useState<EnviromentProps[]>([])
+    const [plants, setPlants] = useState<PlantProps[]>([])
+    const [plantsFiltered, setPlantsFiltered] = useState<PlantProps[]>([])
+    const [environmentSelected, setEnviromentSelected] = useState('all')
+
+    useEffect(() => {
+        async function fetchEnviroment(){
+            const { data } = await api.get('plants_environments')
+            setEnviroments([{
+                key: 'all',
+                title: 'Todos'
+            },
+        ...data
+            ])
+        }
+
+        fetchEnviroment()
+    }, [])
+
+    useEffect(() => {
+        async function fetchPlants(){
+            const { data } = await api.get('plants?_sort=name&_order=asc')
+            setPlants(data)
+        }
+
+        fetchPlants()
+    }, [])
+
+    const handleEnvironmentSelected = (environment: string) => {
+       setEnviromentSelected(environment)
+
+       if(environment === 'all')
+       return setPlantsFiltered(plants)
+
+       const filtered = plants.filter(plant => 
+            plant.environments.includes(environment)
+        )
+
+        setPlantsFiltered(filtered)
+    }
+
     return (
         <View style={styles.container}> 
             <Header />
             <Text style={styles.title}>Em qual ambiente {'\n'}você quer colocar sua planta?</Text>
             <View>
-                <FlatList                    // esse FlatList é equivalente ao map do JavaScript puro 
-                    data={[1, 2, 3, 4, 5]}
-                    renderItem={({item}) => (
-                        <EnviromentBtn title="Cozinha" active/>
-                    )}
+            <ScrollView horizontal style={styles.containerEnviroments}>
+                    {enviroments.map((elem, index) => {
+                        return  <EnviromentBtn active={elem.key === environmentSelected}
+                                                onPress={() => handleEnvironmentSelected(elem.key)}
+                                                key={index}
+                                                title={elem.title} 
+                                />
+                    })}
+            </ScrollView>
+            </View>
 
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={styles.environmentList}
-                />
+            <View style={styles.plants}>
+            <ScrollView style={styles.contentPlants}>
+                    {plantsFiltered.map((elem, index) => {
+                        return  <PlantCardPrimary key={index} data={elem}/>
+                    })}
+            </ScrollView>
             </View>
         </View>
     )
@@ -41,5 +111,22 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         margin: 20
 
+    },
+
+    containerEnviroments: {
+        display: 'flex',
+        flexDirection: 'row',
+        overflow: 'scroll',
+        padding: 20,
+    },
+
+    plants: {
+        flex: 1, 
+        display: 'flex',
+        flexDirection: 'row',
+    },
+
+    contentPlants: {
+        
     }
 })
